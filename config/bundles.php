2003e73efcd46845563f1e0bbbc0fe4bfdfe9d59
Bruno Neves Menezes <brunonm@gmail.com>
2018-02-17 18:02:55 +0000

    Add GraphQL bundle

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+    Overblog\GraphQLBundle\OverblogGraphQLBundle::class => ['all' => true],
 ];
